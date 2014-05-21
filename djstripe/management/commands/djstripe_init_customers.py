470df4a3c1bc90b6ef3394f619705a31830a77ba
Joshua Blount <hello@joshuablount.com>
2014-05-21 16:08:25 -0700

    Try using real Django user model

--- djstripe/management/commands/djstripe_init_customers.py
+++ djstripe/management/commands/djstripe_init_customers.py
@@ -2,15 +2,10 @@
 from __future__ import unicode_literals
 
 from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
 from django.conf import settings
 
 from djstripe.models import Customer
-
-# import the function because just calling the User class
-#   seems to blow up in management commands.
-from djstripe.settings import get_user_model
-
-User = settings.AUTH_USER_MODEL
 
 
 class Command(BaseCommand):
